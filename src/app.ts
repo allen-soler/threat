454672f1a64f68d4be92bcedeed1fdcd51c7bc23
Jorge Allen <j.allensoler@gmail.com>
2023-08-08 14:23:39 +0200

    make it better

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import threatRouter from './routers/threats';
 import './db/mongoose';
 
 const httpsRedirect = require('express-https-redirect');
-const subdomain = require('express-subdomain');
 const cors = require('cors');
 
 const app = express();
@@ -16,22 +15,8 @@ app.use(cors());
 app.use(userRouter);
 app.use(threatRouter);
 
+// If you still want the dashboard routes, you can keep this
 // Dashboard Router (create a separate router for dashboard routes)
-const dashboardRouter = express.Router();
-dashboardRouter.get('/', (req: Request, res: Response) => {
-    console.log("Dashboard route accessed");
 
-    res.send('Dashboard Home Page');
-});
-
-// Using subdomain middleware
-app.use(subdomain('dashboard', dashboardRouter));
-
-// Move the static middleware below the dashboard subdomain middleware
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-
-app.get('*', (req: Request, res: Response) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
 
 export default app;
