80ef81a090f0db3165899473e48e0dd2e9e05672
Jorge Allen <j.allensoler@gmail.com>
2023-08-08 14:46:26 +0200

    make it better

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,11 +1,11 @@
 import validator  from "validator"
 import mongoose from "mongoose"
 
-if (!process.env.MOONGOSEATLAS) {
+if (!process.env.MONGOOSECONATLAS) {
     throw new Error("MOONGOSEATLAS ne pas sur env");
 }
 
-const connection : string | undefined = process.env.MOONGOSEATLAS
+const connection : string | undefined = process.env.MONGOOSECONATLAS
 
 const dbName = 'threats-app'
 
